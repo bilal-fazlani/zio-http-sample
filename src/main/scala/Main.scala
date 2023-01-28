d91db8e1afb0781905602fb564fd82168069c35d
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2023-01-28 16:03:29 +0530

    issue https://github.com/zio/zio-http/issues/1967

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,36 +1,18 @@
-import zio.http.middleware.Cors
-import zio.http.model.{HttpError, Status}
-import zio.http.api.*
-import zio.http.{Server, ServerConfig, ZClient}
-import zio.http.HttpApp
-import zio.http.Response
+import zio.http.{Server, ServerConfig}
+import zio.http.*
+import zio.http.model.Method.GET
 
 object Main extends zio.ZIOAppDefault {
-
-  val cors = Middleware.cors(zio.http.middleware.Cors.CorsConfig())
-
-  val serviceSpec = (SampleApi.spec.toServiceSpec)
-    .middleware(MiddlewareSpec.cors)
-
-  val httpApp =
-    serviceSpec
-      .toHttpApp(SampleEndpoint.endpoint, cors)
-      .mapError {
-        case SampleErrors.InvalidInput =>
-          Response.fromHttpError(HttpError.BadRequest("invalid request"))
-        case SampleErrors.NotAllowed =>
-          Response.fromHttpError(HttpError.Forbidden("no allowed"))
-      }
-
-  // accessible at
-  // http://localhost:9092/api/sample
+  
+  val app = Http.collect {
+    case GET -> !! / "api" / "sample" => Response.ok
+  }
 
   override def run = Server
-    .serve(httpApp)
+    .serve(app)
     .exitCode
     .provide(
       ServerConfig.live(ServerConfig.default.port(9092)),
-      SampleService.live,
       Server.live
     )
 }
--- src/main/scala/SampleEndpoint.scala
+++
@@ -1,7 +0,0 @@
-import zio.*
-
-object SampleEndpoint {
-  val endpoint = SampleApi.spec.implement(input =>
-    ZIO.serviceWithZIO[SampleService](_.serve(input))
-  )
-}
